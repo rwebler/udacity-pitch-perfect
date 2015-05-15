0171f093677904c8f8a7f016b1d080f6dab3facb
Rodrigo Webler <rwebler@gmail.com>
2015-05-14 22:40:03 -0300

    Displaying/hiding recording message and stop button.

--- Pitch Perfect/ViewController.swift
+++ Pitch Perfect/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var recordingLabel: UILabel!
+    
+    @IBOutlet weak var stopButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +24,18 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func recordAction(sender: UIButton) {
+        recordingLabel.hidden = false
+        stopButton.hidden = false;
+        //TODO: Record user's voice
+        println("In recordAudio")
+    }
 
+    @IBAction func stopAction(sender: UIButton) {
+        recordingLabel.hidden = true
+        stopButton.hidden = true;
+        //TODO: Stop recording user's voice
+        println("In stopAction")
+    }
 }
 
