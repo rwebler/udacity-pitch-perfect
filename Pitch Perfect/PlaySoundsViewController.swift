71f498feb78db7736a140532f9c8c626669bf3c3
Rodrigo Webler <rwebler@gmail.com>
2015-05-20 20:38:20 -0300

    With Chipmunk sound

--- Pitch Perfect/PlaySoundsViewController.swift
+++ Pitch Perfect/PlaySoundsViewController.swift
@@ -15,6 +15,9 @@ class PlaySoundsViewController: UIViewController {
     
     var audioPlayer : AVAudioPlayer!
     var receivedAudio : RecordedAudio!
+    
+    var audioEngine : AVAudioEngine!
+    var audioFile : AVAudioFile!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,6 +25,9 @@ class PlaySoundsViewController: UIViewController {
         audioPlayer = AVAudioPlayer(contentsOfURL: receivedAudio.filePathUrl, error: nil)
         audioPlayer.enableRate = true
         audioPlayer.prepareToPlay()
+        
+        audioEngine = AVAudioEngine()
+        audioFile = AVAudioFile(forReading: receivedAudio.filePathUrl, error: nil)
     }
     
     override func viewWillAppear(animated: Bool) {
@@ -52,9 +58,36 @@ class PlaySoundsViewController: UIViewController {
         playAudio(1.5)
     }
 
+    @IBAction func playChipmunkAudio(sender: UIButton) {
+        playAudioWithVariablePitch(1000)
+    }
+
     @IBAction func stopPlaybackButtonAction(sender: UIButton) {
         
         audioPlayer.stop()
+        audioEngine.stop();
+        audioEngine.reset()
+    }
+    
+    func playAudioWithVariablePitch(pitch: Float){
+        audioPlayer.stop()
+        audioEngine.stop()
+        audioEngine.reset()
+        
+        var audioPlayerNode = AVAudioPlayerNode()
+        audioEngine.attachNode(audioPlayerNode)
+        
+        var changePitchEffect = AVAudioUnitTimePitch()
+        changePitchEffect.pitch = pitch
+        audioEngine.attachNode(changePitchEffect)
+        
+        audioEngine.connect(audioPlayerNode, to: changePitchEffect, format: nil)
+        audioEngine.connect(changePitchEffect, to: audioEngine.outputNode, format: nil)
+        
+        audioPlayerNode.scheduleFile(audioFile, atTime: nil, completionHandler: nil)
+        audioEngine.startAndReturnError(nil)
+        
+        audioPlayerNode.play()
     }
     /*
     // MARK: - Navigation
